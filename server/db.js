0103a0106f354da9ad57301b3e67eaa07b43644a
SomiahQ <somiah.alqurashi@gmail.com>
2025-04-29 16:54:28 -0400

    Update

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:4A7Int6cumMt3dJnsN9N8JvF@172.21.50.92:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
